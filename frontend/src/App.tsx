f1085d524d9fbfc1a33422356b0fbc664459c3e4
Milad-Shirvan <miladmagic3@gmail.com>
2026-05-05 11:14:15 +0200

    Services Page
    Did the services page but with out the pictures.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Layout } from './components/layout/Layout';
 import Hem from './pages/hem/page';
+import Tjanster from './pages/hem/services';
 
 function App() {
   return (
@@ -8,8 +9,7 @@ function App() {
       <Layout>
         <Routes>
           <Route path="/" element={<Hem />} />
-          <Route path="/tjanster" element={<div>Tjänster</div>} />
-          <Route path="/portfolj" element={<div>Portfölj</div>} />
+          <Route path="/tjanster" element={<Tjanster />} /><Route path="/portfolj" element={<div>Portfölj</div>} />
           <Route path="/om-oss" element={<div>Om oss</div>} />
           <Route path="/kontakta-oss" element={<div>Kontakt</div>} />
         </Routes>
